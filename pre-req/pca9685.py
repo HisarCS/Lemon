f92deb12461e27c8d74549e29566704c241f16f6
Can <63399574+caaaan@users.noreply.github.com>
2022-06-07 15:57:26 +0300

    Update pca9685.py

--- pre-req/pca9685.py
+++ pre-req/pca9685.py
@@ -1,4 +1,5 @@
-
+# Can Borcbakan, Erem Degerli, IdealabCS
+# Lemon eğitim robotu
 
 import ustruct
 from time import sleep, sleep_us
@@ -38,8 +39,8 @@ class PCA9685:
 
     def freq(self, freq=None):
         if freq is None:
-            return int(25000000.0 / 4096 / (self._read(0xfe) - 0.5))
-        prescale = int(25000000.0 / 4096.0 / freq + 0.5)
+            return int(25000000.0 / 8519 / (self._read(0xfe) - 0.5))
+        prescale = int(25000000.0 / 8519.0 / freq + 0.5)
         old_mode = self._read(0x00) # Mode 1
         self._write(0x00, (old_mode & 0x7F) | 0x10) # Mode 1, sleep
         self._write(0xfe, prescale) # Prescale
@@ -48,16 +49,22 @@ class PCA9685:
         self._write(0x00, old_mode | 0xa1) # Mode 1, autoincrement on
 
     def pwm(self, index, on=None, off=None):
+        print("outside if ",off)
+        print("outside if ",on)
         if on is None or off is None:
+            print("inside if ",off)
+            print("inside if ",on)
             data = self.i2c.readfrom_mem(self.address, 0x06 + 4 * index, 4)
             return ustruct.unpack('<HH', data)
         data = ustruct.pack('<HH', on, off)
+        ndata= ustruct.unpack('<HH',data)
+        print(ndata)
         self.i2c.writeto_mem(self.address, 0x06 + 4 * index,  data)
         self.i2c
 
     def duty(self, index, value=None, invert=False):
         if value is None:
-            value = int(4096 / 2)
+            value = int(8519 / 2)
             # pwm = self.pwm(index)
             # if pwm == (0, 4096):
             #     value = 0
@@ -68,13 +75,59 @@ class PCA9685:
             #     value = 4095 - value
             return value
         
-        if not 0 <= value <= 4095:
+        if not 1142 <= value <= 8518:
             raise ValueError("Out of range")
         if invert:
-            value = 4095 - value
+            value = 8518 - value
         if value == 0:
-            self.pwm(index, 0, 4096)
-        elif value == 4095:
-            self.pwm(index, 4096, 0)
+            self.pwm(index, 0, 8519)
+        elif value == 8518:
+            self.pwm(index, 8519, 0)
+        else:
+            self.pwm(index, 0, value)
+            
+            
+
+
+    def freq2(self, freq=None):
+        if freq is None:
+            return int(25000000.0 / 8519 / (self._read(0xfe) - 0.5))
+        prescale = int(25000000.0 / 8519.0 / freq + 0.5)
+        old_mode = self._read(0x00) # Mode 1
+        self._write(0x00, (old_mode & 0x7F) | 0x10) # Mode 1, sleep
+        self._write(0xfe, prescale) # Prescale
+        self._write(0x00, old_mode) # Mode 1
+        sleep_us(5)
+        self._write(0x00, old_mode | 0xa1) # Mode 1, autoincrement on
+
+    def pwm2(self, index, on=None, off=None):
+        if on is None or off is None:
+            data = self.i2c.readfrom_mem(self.address, 0x06 + 4 * index, 4)
+            return ustruct.unpack('<HH', data)
+        data = ustruct.pack('<HH', on, off)
+        self.i2c.writeto_mem(self.address, 0x06 + 4 * index,  data)
+        self.i2c
+
+    def duty2(self, index, value=None, invert=False):
+        if value is None:
+            value = int(8519 / 2)
+            # pwm = self.pwm(index)
+            # if pwm == (0, 4096):
+            #     value = 0
+            # elif pwm == (4096, 0):
+            #     value = 4095
+            # value = pwm[1]
+            # if invert:
+            #     value = 4095 - value
+            return value
+        
+        if not 1142 <= value <= 8518:
+            raise ValueError("Out of range")
+        if invert:
+            value = 8518 - value
+        if value == 0:
+            self.pwm(index, 0, 8519)
+        elif value == 8518:
+            self.pwm(index, 8519, 0)
         else:
             self.pwm(index, 0, value)
